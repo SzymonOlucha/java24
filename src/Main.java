66c9384440ccc5386c0e5f5a0bd006a298c66dd0
Szymek582 <szymon.olucha@gmail.com>
2019-12-07 11:29:05 +0100

    Pierwszy commit do projektu

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * ..... comment class...
+ *
+ * @autor szymon.olucha@gmail.com
+ * @since 07.12.2019
+ */
+public final class Main {
+    public static void main(String[] args) {
+
+    }
+}
